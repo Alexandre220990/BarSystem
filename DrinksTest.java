e4f41a3b05311803591a757ce76a619505165936
Alexandre220990 <drike.alexandre22@gmail.com>
2021-12-02 15:42:56 +0000

    new

--- DrinksTest.java
+++ DrinksTest.java
@@ -2,18 +2,11 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class DrinksTest {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
                 new BarSystem();
 
-        //Drinks drink1 = new Drinks("Marguerita","Tequilla,Salt,lemon juice",6.00);
-        //Drinks drink2 = new Drinks();
-        //Drinks drink3 = new Drinks("Blue Lagoon","Vodka,blue coracao,cointreau,sprite",6.0);
-
-        //ArrayList<String> allDrinks = new ArrayList<>();
-        //allDrinks.add("Marguerita");
-
-        //JOptionPane.showMessageDialog(null,allDrinks,"test",JOptionPane.INFORMATION_MESSAGE);
     }
+
 }
